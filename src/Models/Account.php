3758fcc9afa0c501ea224b81af8ecb55c4e6eda2
Kartik Talwar <hi@kartikt.com>
2015-12-03 03:09:36 -0500

    Removed unnecessary imports

--- src/Models/Account.php
+++ src/Models/Account.php
@@ -2,17 +2,7 @@
 
 namespace Nylas\Models;
 
-use Nylas\Models\Message;
-use Nylas\Models\Thread;
-use Nylas\Models\Tag;
-use Nylas\Models\File;
-use Nylas\Models\Contact;
-use Nylas\Models\Calendar;
-use Nylas\Models\Draft;
-use Nylas\Models\Event;
-
 use Nylas\NylasAPIObject;
-use Nylas\NylasModelCollection;
 
 
 class Account extends NylasAPIObject {
