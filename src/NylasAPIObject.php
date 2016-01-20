8d61e2b4100eecea9e077d013308396ef550201d
Wojciech <wojtek@omega323.com>
2016-01-20 15:02:41 +0100

    Refactor and add count method to collections

---
+++ src/NylasAPIObject.php
@@ -0,0 +1,27 @@
+<?php namespace Nylas;
+
+class NylasAPIObject {
+
+    public $apiRoot;
+
+    public function __construct() {
+    }
+
+    public function json() {
+        return $this->data;
+    }
+
+    public function _createObject($klass, $namespace, $objects) {
+        $this->data = $objects;
+        $this->klass = $klass;
+        return $this;
+    }
+
+    public function __get($key) {
+        if(array_key_exists($key, $this->data)) {
+            return $this->data[$key];
+        }
+        return NULL;
+    }
+
+}
