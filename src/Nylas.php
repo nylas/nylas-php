2e81a1a6780090ead74726e155fc0c4cf5c33371
Wojciech <wojtek@omega323.com>
2016-01-20 15:42:25 +0100

    Fix

--- src/Nylas.php
+++ src/Nylas.php
@@ -137,7 +137,7 @@ class Nylas {
         $filter['view'] = 'count';
 
         $data = $this->apiClient->get($url, $this->createHeaders() + ['query' => $filter])->json();
-        return $data->count;
+        return $data['count'];
     }
 
     public function getResource($namespace, $klass, $id, $filters) {
